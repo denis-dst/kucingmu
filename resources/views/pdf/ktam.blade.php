6dee8d09fb5e039360f98110c6a1c211e80762fb
denis <denissetiaji.dst@gmail.com>
2026-08-27 14:13:06 +0700

    geser QR

--- resources/views/pdf/ktam.blade.php
+++ resources/views/pdf/ktam.blade.php
@@ -293,7 +293,7 @@
         .box-qr-bottom-right {
             position: absolute;
             top: 62.5%;
-            left: 51.19%;
+            left: 61.19%;
             width: 45.0%;
             height: 31.0%;
             z-index: 15;
